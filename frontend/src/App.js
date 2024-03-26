4faea5410d5b4bcba3926825029f73bbf42cf9a1
kriostt <129346100+kriostt@users.noreply.github.com>
2024-03-25 21:34:04 -0600

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import necessary components and modules
+import { Route, Routes } from "react-router-dom";
+import CreateToDo from "./components/CreateToDo";
+import ReadToDoList from "./components/ReadToDoList";
+import CreateCategory from "./components/CreateCategory";
 
+// main component for application
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {/* route for displaying to do list */}
+      <Route path="/" element={<ReadToDoList />} />
+
+      {/* route for adding a new to do list item */}
+      <Route path="/add" element={<CreateToDo />} />
+
+      {/* route for adding a new category */}
+      <Route path="/add-category" element={<CreateCategory />} />
+    </Routes>
   );
 }
 
+// export the App component
 export default App;
