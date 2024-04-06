56764eaae86a07a08bda939748769065e4183b66
JohnMerin21 <johnmerin2003@gmail.com>
2024-04-05 21:22:35 -0600

    Revert "test"
    This reverts commit 0be7974ab2e7bff442c017c875970dfa9e499f51.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,17 @@
-// Import necessary dependencies
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from "react";
 import './App.css';
-import CalendarPage from './components/CalendarFeature/CalendarPage';
-import Favorites from './components/Favorites';
-import Trash from './components/Trash';
-import HomePage from './components/HomePage';
+import Calendar from "./components/CalendarFeature/Calendar";
 
-// App component
-const App = () => {
+function App(){
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>OrganizeMe</h1>
-          {/* Navigation Links */}
-          <nav>
-            <ul>
-              <li>
-              <Link to="/Home">Homepage</Link>
-
-              </li>
-              <li>
-                <Link to="/Favorites">Favorites</Link>
-              </li>
-              <li>
-                <Link to="/Trash">Trash</Link>
-              </li>
-              <li>
-                <Link to="/Calendar">Calendar</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <main>
-          {/* Routing Switch */}
-          <Switch>
-            <Route path="/Favorites">
-              <Favorites />
-            </Route>
-            <Route path="/Trash">
-              <Trash />
-            </Route>
-            <Route path="/Calendar">
-              <CalendarPage />
-            </Route>
-            <Route path="/Homepage">
-              <HomePage />
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <h1>OrganizeMe Calendar</h1>
+      </header>
+      <main>
+        <Calendar></Calendar>
+      </main>
+    </div>
   );
 };
 
